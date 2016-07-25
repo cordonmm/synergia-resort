ac34bdbe123ad12e0220ff61b9e96b8fd4da7e17
10codeJoseRamon <jterrero@10code.es>
2016-07-25 11:47:08 +0200

    ADD: Popup formulario de comentarios. MOD: routes

---
+++ synergia/laravel/app/views/site/formulario_comentarios.blade.php
@@ -0,0 +1,11 @@
+<form class="form">
+
+    <div class="col-1">
+        <div class="field"><input name="contact-name" type="text" placeholder="Tu nombre" required /></div>
+        <div class="field"><input name="contact-email" type="text" placeholder="Email" required /></div>
+        <div class="field"><input name="contact-phone" type="text" placeholder="Teléfono" /></div>
+        <div class="field"><textarea name="contact-message" placeholder="Mensaje"></textarea></div>
+        <button class="button" name="send" value="sendform">Enviar <i class="icon ion-ios-arrow-right"></i></button>
+    </div>
+
+</form>
